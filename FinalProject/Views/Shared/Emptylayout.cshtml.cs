cda82bbea53ea93e92b605726d67cda21cd22c8b
Diya Bansal <diya28bansal@gmail.com>
2026-07-22 16:09:06 +0530

    finalstd

---
+++ FinalProject/Views/Shared/Emptylayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalProject.Views.Shared
+{
+    public class EmptylayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
